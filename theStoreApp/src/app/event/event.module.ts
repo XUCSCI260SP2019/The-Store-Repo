9085a6bc97a94d336ad8ac4259228825db14767e
Mythologos <bothwells@xavier.edu>
2019-04-07 19:13:53 -0400

    Primed for first test of viewing data from the backend in the frontend.

--- theStoreApp/src/app/event/event.module.ts
+++ theStoreApp/src/app/event/event.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 import { EventComponent } from './event.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [EventComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: EventComponent,
+      }
+    ])
   ],
   exports: [
     EventComponent
