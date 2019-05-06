ee13a5f86584dc9f6b9e45c372373ca316808b72
Mythologos <bothwells@xavier.edu>
2019-05-05 22:56:58 -0400

    Added a few tests to the event.service.spec.ts to try out making tests.

--- theStoreApp/src/app/event/event.service.spec.ts
+++ theStoreApp/src/app/event/event.service.spec.ts
@@ -4,9 +4,28 @@ import { EventService } from './event.service';
 
 describe('EventService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
+  const service: EventService = TestBed.get(EventService);
 
   it('should be created', () => {
-    const service: EventService = TestBed.get(EventService);
     expect(service).toBeTruthy();
+  });
+
+  it('should have getActiveEvents() returning events', (done: DoneFn) => {
+    console.log('Tested getActiveEvents().');
+    service.getActiveEvents().subscribe(newEvents => {
+    expect(newEvents).toBeTruthy();
+    // is that the best predicate for testing? Could this be more specific?
+    done();
+    });
+  });
+
+  it('should have getEventCount() returning a nonzero value in its Promise<number>', (done: DoneFn) => {
+    console.log('Tested getEventCount().');
+    // Karma is complaining that this cannot call a .then() within a sync request. Why?
+    // The Angular documentation seems to do it just fine.
+    service.getEventCount().then(num => {
+      expect(num).toBeGreaterThan(0);
+    });
+    done();
   });
 });
